a52b327dc46b5c44f21bc7263c66268545c15303
Pedro Jodar <pedrojodargabarron@gmail.com>
2023-02-13 11:14:39 +0100

    revision

--- data/querys/querys.sql
+++ data/querys/querys.sql
@@ -1,4 +1,6 @@
 ---------- QUERY 1 -------------
+Agrupamos por campeones para sacar la buidl completa en partida.
+
 SELECT campeon, 
 	   CONCAT(item_1, item_2, item_3, 
               item_4, item_5, item_6)
@@ -11,6 +13,7 @@ GROUP BY campeon
 
 
 ---------- QUERY 2 -------------
+Comprobamos la disponibilidad de campeones gratuitos de la semana.
 
 SELECT campeon, runas, hechizos
 FROM campeones as c
@@ -19,4 +22,17 @@ LEFT JOIN disponibilidad as d
 ON c.riot_id = d.riot_id
 
 WHERE disponibilidad = 'disponible'
+;
+
+
+
+
+---------- QUERY 3 -------------
+Comprobamos que campeones son fuertes contra el campeón que se especifique en el WHERE
+
+SELECT campeon
+FROM campeones as c
+
+
+WHERE fuerte LIKE '%Aatrox%'
 ;
--- src/funcion.py
+++ src/funcion.py
@@ -11,21 +11,21 @@ from bs4 import BeautifulSoup
 
 def extraer_datos(url):
     
-    driver=webdriver.Chrome(PATH)
+    driver=webdriver.Chrome(PATH)                # Inicializamos el driver 
     driver.get(url)
     
     
     data = [e.get_attribute('alt') for e in driver.find_elements(By.TAG_NAME, 'img') if e.get_attribute('alt')!='']
-    
+                                                 # Filtramos el dato para quedarnos con lo que nos interesa en cada caso
     nombre = data[0][6:]
     #posicion = data[1]
     
     if data[2] == 'Top' or data[2] == 'Mid' or data[2] == 'Jungla' or data[2] == 'ADC' or data[2] == 'Support':
-        fuerte = data[3:8]
-        fuerte_str = ''
+        fuerte = data[3:8]                       # Metemos el condicional ya que los datos extraidos de los diferentes url, 
+        fuerte_str = ''                          # tienen dos posibles estructuras
         for e in fuerte:
             fuerte_str += e + ' '
-        fuerte_str = fuerte_str[:-1]
+        fuerte_str = fuerte_str[:-1]             # Convertimos los datos sacados en una string para que pueda ser más manejable
             
         debil = data[8:13]
         debil_str = ''
@@ -53,7 +53,7 @@ def extraer_datos(url):
         item_6 = data[22]
     
     
-    else:
+    else:                                                 # Condicional para nuestro segundo tipo de estructura
         fuerte = data[2:7]
         fuerte_str = ''
         for e in fuerte:
@@ -87,9 +87,8 @@ def extraer_datos(url):
         
     
     
-    data_clean = {'campeon':nombre,
-                  #'posicion':posicion,
-                  'fuerte':fuerte_str,
+    data_clean = {'campeon':nombre,                       # Preparamos nuestros datos scrapeados en un diccionario donde la key
+                  'fuerte':fuerte_str,                    # será el nombre de la columna y el value una lista con nuestros datos
                   'debil': debil_str,
                   'hechizos':hechizos_str,
                   'obj_ini':obj_ini_str,
@@ -109,9 +108,9 @@ def extraer_datos(url):
 
 
 
-def extraer_runas(url):
-    
-    html = requests.get(url).text
+def extraer_runas(url):                                       # Función orientada a sacar otro tipo de dato
+                                                              # En esta ocasión selenium no nos bastaba para concretar este dato
+    html = requests.get(url).text                                    
 
     sopa = BeautifulSoup(html, 'html.parser')
     
@@ -119,19 +118,19 @@ def extraer_runas(url):
     
     lista = [e.text for e in runas]
     
-    lista = lista[:7]
+    lista = lista[:7]                                         # Filtramos el dato
     word = ''
     
     for e in lista:
         word += e + ',' + ' '
     word = word[:-1]
     
-    nombre = sopa.find('h1', {'class':'text-2xl truncate'}).text[6:]
-    
+    nombre = sopa.find('h1', {'class':'text-2xl truncate'}).text[6:]     # Extraemos también el nombre de nuestros campeones
+                                                                         # para hacer el merge más adelante
     
     data = {'campeon':nombre,
-            'runas':word}
-    
+            'runas':word}                                                # Preparamos nuestro dato en un diccionario para hacer
+                                                                         # para hacer el dataframe
     return data
 
 
